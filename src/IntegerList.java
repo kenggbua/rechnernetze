744423add90cfcc50c5574bada9827a9fb045937
kenggbua <kevin.wiltschnig@hotmail.com>
2019-10-19 19:53:56 +0200

    Initial commit

---
+++ src/IntegerList.java
@@ -0,0 +1,18 @@
+public class IntegerList {
+
+
+
+
+    private int[] data;
+    protected int size;
+    private static final int DEFAULT_SIZE = 10;
+    public IntegerList( ) {
+        data = new int[DEFAULT_SIZE];
+    }
+    public IntegerList(IntegerList toCopy) {
+        IntegerList list = new IntegerList();
+
+
+
+    }
+}
